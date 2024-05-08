004496e65378f3e729bfc861b012be3c67335cc6
Agustin Urien <agus.urien2@gmail.com>
2024-05-08 14:56:01 -0300

    finish proyect

--- app/layout.js
+++ app/layout.js
@@ -8,11 +8,6 @@ export const metadata = {
   title: "Kinode | Agustin Urien",
   description: "The technical home website challenge for Kinode.",
   canonicalUrl: 'https://kinode-task.vercel.app/',
-  icons: {
-    icon: ['/favicon.ico?v=4'],
-    apple: ['/apple-touch-icon.png?v=4'],
-    shortcut: ['/apple-touch-icon.png']
-  }
 };
 
 export default function RootLayout({ children }) {
